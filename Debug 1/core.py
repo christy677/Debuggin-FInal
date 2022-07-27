635d129a2263478038296ec2c8a0b28614a03f63
christy677 <christy148@gmail.com>
2022-07-27 09:51:16 -0500

    cals

--- Debug 1/core.py
+++ Debug 1/core.py
@@ -1,11 +1,11 @@
 # From the list below, convert all negative numbers into positive numbers and add them into a new array.
 
-data = [-18, 5, 913, -83, -1, 8, -94]
-newData = []
-for x in data:
-    if '-' in str(x):
-        n = x.replace('-')
-        newData.append(int(n))
-    else:
-        newData.append('-')
-print(newData)
+data = [-18 *-1, 5, 913, -83 *-1,-1*-1, 8, -94*-1]
+
+new_1=[18 + 5 + 913 + 83 + 1 + 8 + 94]
+
+print (new_1)
+
+
+
+
