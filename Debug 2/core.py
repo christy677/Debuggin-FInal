5a5531a13e21e52a9dfb1bb207ce52ff9220b072
christy677 <christy148@gmail.com>
2022-07-27 10:09:01 -0500

    debug2

--- Debug 2/core.py
+++ Debug 2/core.py
@@ -4,17 +4,20 @@ d1 = {'a': 1, 'b': 2, 'c': 3}
 d2 = {'name': 'Zach', 'color': 'red', 'age': 22}
 d3 = {'reptile': 'lizard', 'mammal': 'dog', 'aquatic': 'fish'}
 
+# print (d1,d2, d3)
+
+def main():
+    inv1 = d1
+    inv2 = d2
+    inv3 = d3
+    print(inv1, inv2, inv3)
+    
 def invert(dictionary):
     temp = {}
     for keys in dictionary:
         temp[keys[dictionary]] += keys
     return temp
 
-def main():
-    inv1 = invert(d1)
-    inv2 = invert(d2)
-    inv3 = invert(d3)
-    print(inv1, inv2, inv3)
-
 if __name__ in '__main__':
     main()
+    
